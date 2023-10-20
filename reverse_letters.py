d20e9dac2309a92095ffe09a4812f8318b969aaa
Prince Tash <jamesngangatash@gmail.com>
2023-10-20 14:20:50 +0300

    updated

--- reverse_letters.py
+++ reverse_letters.py
@@ -2,7 +2,7 @@
 string and works its way backwards to the first character in the string,
 printing each letter on a separate line, except backwards."""
 
-
+#forward printing
 fruit = "apple"
 index = 0
 while index < len(fruit):
@@ -13,3 +13,13 @@ while index < len(fruit):
 
 # for letter in fruit:
 #     print(letter)
+
+
+#backward printing
+fruit = "apple"
+index = len(fruit) - 1  # Start at the last character's index
+while index >= 0:  # Loop until the first character's index (0)
+    letter = fruit[index]
+    print(letter)
+    index = index - 1  # Move backwards through the string
+
