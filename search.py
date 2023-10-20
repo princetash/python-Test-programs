ae8b070ae1e63c1446299889fc5ab8e827d25a1d
Prince Tash <jamesngangatash@gmail.com>
2023-10-20 14:27:20 +0300

    added third parameter

--- search.py
+++ search.py
@@ -26,5 +26,22 @@ def find(word, letter):
 
 
 print(find("james", "m"))
+
+
 """As an exercise, modify find so that it has a third parameter, the index in word where it
 should start looking."""
+
+def find(word, letter, start_index):
+    index = start_index
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+
+# Example usage:
+word = "james"
+letter = "m"
+start_index = 1  # You can specify the starting index here
+result = find(word, letter, start_index)
+print(result)
